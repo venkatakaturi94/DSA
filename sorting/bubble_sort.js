5e93d1e743a7a1e7865feec5f709dfac67770c93
venkatakaturi <venkatakaturi94@gmail.com>
2021-01-07 10:12:05 +0530

    Added Bubble Sort 1

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -1,3 +1,19 @@
+PseudoCode
+
+Start looping from with a variable called i the end of the array towards the beginning
+
+Start an inner loop with a variable called j from the beginning until i - 1
+
+If arr[j] is greater than arr[j+1], swap those two values!
+
+Return the sorted array
+
+
+
+
+
+
+
 // Optimized BubbleSort with noSwaps
 function bubbleSort(arr){
   var noSwaps;
